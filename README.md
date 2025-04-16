eb7eda968e50f2ce73defb3d36a43de4bb36cea9
Phil Rossiter <phil_rossiter@hotmail.com>
2025-04-16 17:27:08 +0100

    Initial version of calculation process for testing

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # nearest_distances
-Calculate nearest distances for locations using eastings &amp; northings
+Calculate nearest distances for locations using eastings & northings using DuckDB
+
+## Steps:
+1. Download latest DuckDb version: [DuckDB](https://duckdb.org/docs/installation/)
+2. Extract and save in an executable folder (e.g. c:\tmp)
+3. Open command line in folder and run DuckDB. _This will use an in-memory DB_
+4. Prepare your data with, at least:
+   - a) An "id" key column to uniquely identify rows and join on
+   - b) "easting" and "northing" columns to calculate distances
+   - c) Any other columns you need, these will be exported in the results
+   - Save this data in the DuckDB executable folder
+5. Adjust the number of nearest matches you want using the `nearest_matches` variable
+6. Copy/paste the SQL code into DuckDB and let it run. It may take a while if you have lots of records (tested with ~20k)
---
+++ duck_db_pythagoras_calculations.sql
@@ -0,0 +1,46 @@
+-- See README.md for usage instructions
+
+-- How many nearest matches do you want (in descending order)
+SET VARIABLE nearest_matches = 5;
+
+DROP TABLE matching_source;
+
+CREATE TABLE matching_source AS
+    SELECT * FROM read_csv_auto('matching_source.csv', HEADER = True);
+
+-- Distance calculation and output
+CREATE OR REPLACE VIEW match_output_v AS
+WITH distances AS (
+    SELECT
+        s.*,
+        p.id  as matched_id,
+        sqrt(((s.easting - p.easting)^2) + (s.northing - p.northing)^2) AS distance_away
+    FROM
+        matching_source s
+        INNER JOIN matching_source p
+            ON s.id <> p.id
+),
+ordered_distances AS (
+    SELECT
+        p.id,
+        p.matched_id,
+        p.distance_away,
+        -- Order these distance by school
+        row_number() OVER (PARTITION BY p.id ORDER BY distance_away) AS distance_order
+    FROM
+        distances p
+)
+SELECT
+    d.*
+FROM
+    distances d
+    INNER JOIN ordered_distances m
+        ON d.id = m.id
+        AND d.matched_id = m.matched_id
+        AND m.distance_order <= getvariable('nearest_matches')
+ORDER BY
+    d.id,
+    m.distance_away
+;
+
+COPY (SELECT * FROM match_output_v) TO 'match_output_v.csv' (HEADER, DELIMITER ',');
